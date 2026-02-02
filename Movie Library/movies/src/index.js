a3c2dc10aec3a60475a22009a4cee14eb57825cc
VENKATESH CHOPPADHANDI <119341734+Venkatesh66666@users.noreply.github.com>
2026-02-02 16:58:13 +0530

    index.js

--- Movie Library/movies/src/index.js
+++ Movie Library/movies/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
-import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import { HashRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
@@ -25,60 +25,57 @@ import MovieSearchPage from "./pages/movieSearchPage";
 import SearchResultMoviesPage from "./pages/searchResultMoviesPage";
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 360000,
-            refetchInterval: 360000,
-            refetchOnWindowFocus: false
-        },
-    },
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false
+    }
+  }
 });
 
-
 const App = () => {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <SiteHeader />
-                <MoviesContextProvider>
-                    <Routes>
-                        <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
-                        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <SiteHeader />
+        <MoviesContextProvider>
+          <Routes>
+            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
+            <Route path="/reviews/:id" element={<MovieReviewPage />} />
 
-                        <Route path="/movies/discover/page/:page" element={<HomePage />} />
-                        <Route path="/movies/discover" element={<HomePage />} />
-                        <Route path="/movies/search" element={<MovieSearchPage />} />
-                        <Route path="/movies/search/:title" element={<SearchResultMoviesPage />} />
-                        <Route path="/movies/search/:title/page/:page" element={<SearchResultMoviesPage />} />
-                        <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-                        <Route path="/movies/watchlist" element={<WatchListMoviesPage />} />
-                        <Route path="/movies/upcoming/page/:page" element={<UpcomingMoviesPage />} />
-                        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
-                        <Route path="/movies/trending/today/page/:page" element={<TrendingTodayMoviesPage />} />
-                        <Route path="/movies/trending/today" element={<TrendingTodayMoviesPage />} />
-                        <Route path="/movies/playing/page/:page" element={<PlayingMoviesPage />} />
-                        <Route path="/movies/playing" element={<PlayingMoviesPage />} />
-                        <Route path="/movies/latest" element={<LatestMoviePage />} />
-                        <Route path="/movies/:id/similar" element={ <SimilarMoviesPage /> } />
-                        <Route path="/movies/:id/credits" element={ <MovieCreditsPage /> } />
-                        <Route path="/movies/:id" element={<MoviePage />} />
+            <Route path="/movies/discover/page/:page" element={<HomePage />} />
+            <Route path="/movies/discover" element={<HomePage />} />
+            <Route path="/movies/search" element={<MovieSearchPage />} />
+            <Route path="/movies/search/:title" element={<SearchResultMoviesPage />} />
+            <Route path="/movies/search/:title/page/:page" element={<SearchResultMoviesPage />} />
+            <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+            <Route path="/movies/watchlist" element={<WatchListMoviesPage />} />
+            <Route path="/movies/upcoming/page/:page" element={<UpcomingMoviesPage />} />
+            <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+            <Route path="/movies/trending/today/page/:page" element={<TrendingTodayMoviesPage />} />
+            <Route path="/movies/trending/today" element={<TrendingTodayMoviesPage />} />
+            <Route path="/movies/playing/page/:page" element={<PlayingMoviesPage />} />
+            <Route path="/movies/playing" element={<PlayingMoviesPage />} />
+            <Route path="/movies/latest" element={<LatestMoviePage />} />
+            <Route path="/movies/:id/similar" element={<SimilarMoviesPage />} />
+            <Route path="/movies/:id/credits" element={<MovieCreditsPage />} />
+            <Route path="/movies/:id" element={<MoviePage />} />
 
-                        <Route path="/similar" element={ <SimilarMoviesPage /> } />
-                        <Route path="/similar/:id" element={ <SimilarMoviesPage /> } />
+            <Route path="/people/popular/page/:page" element={<PopularPeoplePage />} />
+            <Route path="/people/popular" element={<PopularPeoplePage />} />
+            <Route path="/people/:id" element={<PersonPage />} />
+            <Route path="/people/:id/credits/movie" element={<MovieCreditsForPersonPage />} />
 
-                        <Route path="/people/popular/page/:page" element={<PopularPeoplePage />} />
-                        <Route path="/people/popular" element={<PopularPeoplePage />} />
-                        <Route path="/people/:id" element={<PersonPage />} />
-                        <Route path="/people/:id/credits/movie" element={<MovieCreditsForPersonPage />} />
-
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="*" element={ <Navigate to="/" /> } />
-                    </Routes>
-                </MoviesContextProvider>
-            </BrowserRouter>
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-    );
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </MoviesContextProvider>
+      </HashRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
-const rootElement = createRoot( document.getElementById("root") )
+
+const rootElement = createRoot(document.getElementById("root"));
 rootElement.render(<App />);
